8d0fe4a6ebf7fb82d2ffd093637b10440b3cca6c
IvanSPb85 <Vanya85spb@yandex.ru>
2023-04-10 20:57:20 +0300

    feat: Director.class

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Director.java
@@ -0,0 +1,14 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+@Builder
+public class Director {
+    private Long id;
+    @NotBlank(message = "Имя директора не может быть пустым.")
+    private String name;
+}
