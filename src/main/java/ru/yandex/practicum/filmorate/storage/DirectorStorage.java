9e771881ee0e07b76f9c18caa35ccfb67a36c36a
IvanSPb85 <Vanya85spb@yandex.ru>
2023-04-10 23:40:44 +0300

    feat: interface DirectorStorage

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/DirectorStorage.java
@@ -0,0 +1,16 @@
+package ru.yandex.practicum.filmorate.storage;
+
+import ru.yandex.practicum.filmorate.model.Director;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public interface DirectorStorage {
+    Collection<Director> findAll();
+
+    Optional<Director> findById(Long id);
+
+    Optional<Director> createDirector(Director director);
+
+    Optional<Director> updateDirector(Director director);
+}
