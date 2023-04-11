d4d1a2194511c9412ac4adcf47c0dd0cdbd39149
IvanSPb85 <Vanya85spb@yandex.ru>
2023-04-11 09:09:56 +0300

    feat: DirectorDbStorage

--- src/main/java/ru/yandex/practicum/filmorate/service/DirectorService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/DirectorService.java
@@ -1,29 +1,54 @@
 package ru.yandex.practicum.filmorate.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.exception.IncorrectObjectIdException;
+import ru.yandex.practicum.filmorate.exception.ValidateException;
 import ru.yandex.practicum.filmorate.model.Director;
+import ru.yandex.practicum.filmorate.storage.DirectorStorage;
 
 import java.util.Collection;
+import java.util.Optional;
+
 
 @Service
+@RequiredArgsConstructor
 public class DirectorService {
+    private final DirectorStorage storage;
 
     public Collection<Director> findAll() {
-        return null;
+        return storage.findAll();
     }
 
     public Director findById(Long id) {
-        return null;
+        Optional<Director> optionalDirector = storage.findById(id);
+        if (optionalDirector.isEmpty()) {
+            throw new IncorrectObjectIdException(String.format("Директор %d не найден.", id));
+        }
+        return optionalDirector.get();
     }
 
     public Director createDirector(Director director) {
-        return director;
+        if (director.getName().isBlank()) {
+            throw new ValidateException("Имя директора не может быть пустым.");
+        }
+        Optional<Director> optionalDirector = storage.createDirector(director);
+        if (optionalDirector.isEmpty()) {
+            throw new IncorrectObjectIdException(String.format("Директор %s не создан.", director.getName()));
+        }
+        return optionalDirector.get();
     }
 
     public Director updateDirector(Director director) {
-        return director;
+        Optional<Director> optionalDirector = storage.updateDirector(director);
+        if (optionalDirector.isEmpty()) {
+            throw new IncorrectObjectIdException(String.format("Директор %d %s не обновлен.",
+                    director.getId(), director.getName()));
+        }
+        return optionalDirector.get();
     }
 
     public void deleteDirector(Long id) {
+        storage.deleteDirector(id);
     }
 }
