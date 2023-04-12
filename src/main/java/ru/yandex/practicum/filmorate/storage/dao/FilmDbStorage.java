46696e78d253057d68b9221a5f62bfaaa63938bd
IvanSPb85 <Vanya85spb@yandex.ru>
2023-04-12 08:15:43 +0300

    refactor: Checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/dao/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/dao/FilmDbStorage.java
@@ -184,7 +184,8 @@ public class FilmDbStorage implements FilmStorage {
                         "ORDER BY F.RELEASE_DATE;",
                 new MapSqlParameterSource()
                         .addValue("DIRECTOR_ID", directorId),
-                filmMapper);    }
+                filmMapper);
+    }
 
     @Override
     public Collection<Film> findFilmsDirectorByLikes(Long directorId) {
