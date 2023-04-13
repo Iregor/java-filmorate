0ad28aaea54d2f587b86d5afd08e02c4c7098878
AGolykh <a.golykh@yandex.ru>
2023-04-13 10:33:01 +0300

    fix: codeStyle

--- src/test/java/ru/yandex/practicum/filmorate/storage/dao/FilmDbStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/storage/dao/FilmDbStorageTest.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
--- src/test/java/ru/yandex/practicum/filmorate/storage/dao/MpaDbStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/storage/dao/MpaDbStorageTest.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.storage.dao;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import ru.yandex.practicum.filmorate.model.Mpa;
