bd5f4621ac60d1577918d4e0a231649e75e3d3bb
Nikita <nikita.pro.pro@yandex.ru>
2023-04-15 12:18:16 +0300

    feat: add interface EventStorage

--- src/main/java/ru/yandex/practicum/filmorate/storage/EventStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/EventStorage.java
@@ -2,12 +2,13 @@ package ru.yandex.practicum.filmorate.storage;
 
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.Event;
+import ru.yandex.practicum.filmorate.model.EventType;
+import ru.yandex.practicum.filmorate.model.Operation;
 
 import java.util.Collection;
 
 @Component
 public interface EventStorage {
     Collection<Event> getFeed(Long id);
-
-    void addEvent(Event event);
+    void createEvent(Long userId, Long entityId, EventType eventType, Operation operation);
 }
