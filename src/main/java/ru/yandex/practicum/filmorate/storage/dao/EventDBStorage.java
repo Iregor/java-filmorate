aab6063b87bb1bcd537c39c26f28a483b1b71f6e
Nikita <nikita.pro.pro@yandex.ru>
2023-04-15 12:19:38 +0300

    feat: add EventDBStorage

--- src/main/java/ru/yandex/practicum/filmorate/storage/dao/EventDBStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/dao/EventDBStorage.java
@@ -2,44 +2,39 @@ package ru.yandex.practicum.filmorate.storage.dao;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
-import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.Event;
-import ru.yandex.practicum.filmorate.model.enums.EventType;
-import ru.yandex.practicum.filmorate.model.enums.Operation;
+import ru.yandex.practicum.filmorate.model.EventType;
+import ru.yandex.practicum.filmorate.model.Operation;
 import ru.yandex.practicum.filmorate.storage.EventStorage;
 
-import javax.sql.DataSource;
-import java.time.Instant;
+import java.sql.PreparedStatement;
+import java.sql.Timestamp;
 import java.util.Collection;
-
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 @Component("EventDBStorage")
 @Slf4j
 @RequiredArgsConstructor
 public class EventDBStorage implements EventStorage {
     private final JdbcTemplate jdbcTemplate;
-    private final DataSource dataSource;
-
-    @Autowired
-    public EventDBStorage(DataSource dataSource) {
-        this.dataSource = dataSource;
-        this.jdbcTemplate = new JdbcTemplate(dataSource);
-    }
 
     static final RowMapper<Event> eventMapper = (rs, rowNum) -> Event.builder()
             .eventId(rs.getInt("EVENT_ID"))
-            .userId(rs.getLong("USER_ID"))
-            .timestamp(rs.getLong("TIMESTAMP"))
+            .userId(rs.getInt("USER_ID"))
+            .timestamp(Timestamp.valueOf(rs.getTimestamp("TIMESTAMP").toLocalDateTime()))
             .eventType(EventType.valueOf(rs.getString("EVENT_TYPE")))
             .operation(Operation.valueOf(rs.getString("OPERATION")))
-            .entityId(rs.getLong("ENTITY_ID"))
+            .entityId(rs.getInt("ENTITY_ID"))
             .build();
+
 
     @Override
     public Collection<Event> getFeed(Long id) {
@@ -47,16 +42,46 @@ public class EventDBStorage implements EventStorage {
         return jdbcTemplate.query(query, new Object[]{id}, eventMapper);
     }
 
-    @Override
-    public void addEvent(Event event) {
-        SqlParameterSource param = new MapSqlParameterSource()
-                .addValue("USER_ID", event.getUserId())
-                .addValue("EVENT_TYPE", event.getEventType().name())
-                .addValue("OPERATION", event.getOperation())
-                .addValue("ENTITY_ID", event.getEntityId())
-                .addValue("TIMESTAMP", Instant.now().toEpochMilli());
-        SimpleJdbcInsert insert = new SimpleJdbcInsert(dataSource).withTableName("FEEDS")
-                .usingGeneratedKeyColumns("EVENT_ID");
-        insert.execute(param);
+    public void createEvent(Long userId, Long entityId, EventType eventType, Operation operation) {
+        log.info("Добавление события в ленту пользователя с с id-{} ", userId);
+        String sql = "INSERT INTO FEEDS (" +
+                "TIMESTAMP" +
+                ", USER_ID" +
+                ", ENTITY_ID" +
+                ", EVENT_TYPE" +
+                ", OPERATION" +
+                ") " +
+                "VALUES (?,?,?,?,?)";
+
+        jdbcTemplate.update(connection -> {
+            PreparedStatement stmt = connection.prepareStatement(sql, new String[]{"event_id"});
+            Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+            stmt.setTimestamp(1, timestamp);
+            stmt.setInt(2, Math.toIntExact(userId));
+            stmt.setInt(3, Math.toIntExact(entityId));
+            stmt.setString(4, eventType.toString());
+            stmt.setString(5, operation.toString());
+            return stmt;
+        });
+    }
+
+    private Optional<Event> findById(long id) {
+        try {
+            String query = "SELECT * FROM FEEDS WHERE EVENT_ID = :EVENT_ID;";
+            SqlParameterSource params = new MapSqlParameterSource("EVENT_ID", id);
+            return Optional.ofNullable(jdbcTemplate.queryForObject(query, params.getParameterNames(), eventMapper));
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
+    }
+
+    private Map<String, Object> getFeedParams(Event event) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("TIMESTAMP", event.getTimestamp());
+        params.put("USER_ID", event.getUserId());
+        params.put("EVENT_TYPE", event.getEventType().name());
+        params.put("OPERATION", event.getOperation().name());
+        params.put("ENTITY_ID", event.getEntityId());
+        return params;
     }
 }
