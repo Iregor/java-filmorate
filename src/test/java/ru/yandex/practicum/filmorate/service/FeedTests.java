ee9a4e622fc85995f6559d65769cf85f60422db5
Nikita <nikita.pro.pro@yandex.ru>
2023-04-15 16:33:21 +0300

    fix: codestyle

--- src/test/java/ru/yandex/practicum/filmorate/service/FeedTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/service/FeedTests.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.service;
 
 import lombok.RequiredArgsConstructor;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
