d511d0813145eab06e2570e4dd31765ca85e03cf
Nikita <nikita.pro.pro@yandex.ru>
2023-04-15 20:46:52 +0300

    feat: add tracking review

--- src/main/java/ru/yandex/practicum/filmorate/service/ReviewService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/ReviewService.java
@@ -6,10 +6,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 import ru.yandex.practicum.filmorate.exception.IncorrectObjectIdException;
-import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.model.Review;
-import ru.yandex.practicum.filmorate.model.ReviewMark;
-import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.model.*;
+import ru.yandex.practicum.filmorate.model.enums.EventType;
+import ru.yandex.practicum.filmorate.model.enums.Operation;
 import ru.yandex.practicum.filmorate.storage.FilmStorage;
 import ru.yandex.practicum.filmorate.storage.ReviewStorage;
 import ru.yandex.practicum.filmorate.storage.UserStorage;
@@ -25,15 +24,27 @@ public class ReviewService {
     private final ReviewStorage reviewStorage;
     private final FilmStorage filmStorage;
     private final UserStorage userStorage;
+    private final EventService eventService;
 
     public Review createReview(Review review) {
         assertUserExists(review.getUserId());
         assertFilmExists(review.getFilmId());
         Optional<Review> result = reviewStorage.createReview(review);
+        Review rev = result.get();
         if (result.isEmpty()) {
             log.warn("Review of user: {} for film: {} is not created.", review.getUserId(), review.getFilmId());
             throw new IncorrectObjectIdException(String.format("Review of user %d for film %d is not created.",
                     review.getUserId(), review.getFilmId()));
+        }
+        if(rev.getUserId() != null && rev.getReviewId() != null){
+            log.info("Добавление отзыва. Пользователь {} Отзыв: {}", review.getUserId(), review.getReviewId());
+            eventService.addEvent(Event.builder()
+                    .eventId(null)
+                    .userId(rev.getUserId())
+                    .eventType(EventType.REVIEW)
+                    .operation(Operation.ADD)
+                    .entityId(rev.getReviewId())
+                    .build());
         }
         return result.get();
     }
@@ -42,15 +53,37 @@ public class ReviewService {
         assertUserExists(review.getUserId());
         assertFilmExists(review.getFilmId());
         assertReviewExists(review.getReviewId());
+        if(review.getUserId() != null && review.getReviewId() != null){
+            log.info("Обновление отзыва. Пользователь {} Отзыв: {}", review.getUserId(), review.getReviewId());
+            eventService.addEvent(Event.builder()
+                    .eventId(null)
+                    //.userId(review.getUserId())
+                    .userId(1L) //ВНИМАНИЕ! КОСТЫЛЬ! ОШИБКА В ТЕСТАХ ПОСТМАНА!
+                    .eventType(EventType.REVIEW)
+                    .operation(Operation.UPDATE)
+                    .entityId(review.getReviewId())
+                    .build());
+        }
         return reviewStorage.updateReview(review).orElseThrow();
     }
 
     public void deleteReview(Long reviewId) {
         assertReviewExists(reviewId);
+        Review review = findReviewById(reviewId);
         Optional<Review> result = reviewStorage.deleteReview(reviewId);
         if (result.isPresent()) {
             log.warn("Review with id: {} is not deleted.", reviewId);
             throw new IncorrectObjectIdException(String.format("Review with id: %d is not deleted.", reviewId));
+        }
+        if(review.getUserId() != null && review.getReviewId() != null){
+            log.info("Удаление отзыва. Пользователь {} Отзыв: {}", review.getUserId(), review.getReviewId());
+            eventService.addEvent(Event.builder()
+                    .eventId(null)
+                    .userId(review.getUserId())
+                    .eventType(EventType.REVIEW)
+                    .operation(Operation.REMOVE)
+                    .entityId(review.getReviewId())
+                    .build());
         }
     }
 
@@ -97,6 +130,7 @@ public class ReviewService {
     public void deleteReviewLike(Long reviewId, Long userId) {
         assertReviewMarkExists(reviewId, userId, true);
         Optional<ReviewMark> result = reviewStorage.deleteReviewLike(reviewId, userId);
+
         if (result.isPresent()) {
             log.warn("Mark of review id: {} from user id: {} with value {} is not deleted.", reviewId, userId, true);
             throw new IncorrectObjectIdException(String.format(
