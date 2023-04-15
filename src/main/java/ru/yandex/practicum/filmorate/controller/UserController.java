77509c9e661200413b131473a52c1c6f7bcb16f2
Nikita <nikita.pro.pro@yandex.ru>
2023-04-15 14:14:28 +0300

    fix: Changing the logic of adding and deleting to the feed

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import lombok.RequiredArgsConstructor;
-
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Film;
@@ -11,6 +10,7 @@ import ru.yandex.practicum.filmorate.service.UserService;
 
 import javax.validation.Valid;
 import java.util.Collection;
+
 
 @Slf4j
 @RestController
--- src/main/java/ru/yandex/practicum/filmorate/model/Event.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Event.java
@@ -1,8 +1,6 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 import lombok.experimental.FieldDefaults;
 
 import javax.validation.constraints.NotEmpty;
@@ -10,15 +8,19 @@ import java.io.Serializable;
 import java.sql.Timestamp;
 
 @Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class Event implements Serializable {
     Timestamp timestamp;
-    int eventId;
+    Integer eventId;
     @NotEmpty(message = "Идентификатор пользователя не может быть пустым.")
-    int userId;
+    Long userId;
     @NotEmpty(message = "Идентификатор сущности не может быть пустым.")
-    int entityId;
+    Long entityId;
     @NotEmpty(message = "Тип события не может быть пустым.")
     EventType eventType;
     @NotEmpty(message = "Тип операции не может быть пустым.")
