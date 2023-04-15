32daba7baf29fdfd5b5a86f5e0ecaf04587abf42
Nikita <nikita.pro.pro@yandex.ru>
2023-04-15 12:16:45 +0300

    feat: add FeedController

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FeedController.java
@@ -0,0 +1,27 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.filmorate.model.Event;
+import ru.yandex.practicum.filmorate.service.EventService;
+
+import java.util.Collection;
+
+@Slf4j
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class FeedController {
+    private final EventService eventService;
+
+    @GetMapping("/{id}/feed")
+    public Collection<Event> getFeed(@PathVariable Long id){
+        Collection<Event> events = eventService.getFeed(id);
+        log.info("Запрошена лента событий пользователя с id {}: {}", id, events);
+        return events;
+    }
+}
