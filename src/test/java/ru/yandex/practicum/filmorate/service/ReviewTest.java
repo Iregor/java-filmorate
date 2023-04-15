5db5ab5874a9f8055252c212daddc7c206687d0f
Nikita <nikita.pro.pro@yandex.ru>
2023-04-15 21:10:31 +0300

    fix: tests

--- src/test/java/ru/yandex/practicum/filmorate/service/ReviewTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/service/ReviewTest.java
@@ -14,7 +14,6 @@ import ru.yandex.practicum.filmorate.model.*;
 
 import java.time.LocalDate;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
