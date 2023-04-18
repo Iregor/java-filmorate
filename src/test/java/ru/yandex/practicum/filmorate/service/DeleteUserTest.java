2a2053f6dea57686e775a3a63abc98aadc9b4a01
AGolykh <a.golykh@yandex.ru>
2023-04-18 14:26:55 +0300

    fix: checkStyle

--- src/test/java/ru/yandex/practicum/filmorate/service/DeleteUserTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/service/DeleteUserTest.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
-import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.util.Collection;
